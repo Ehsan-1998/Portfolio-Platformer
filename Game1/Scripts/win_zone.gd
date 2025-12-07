35c5a9d9704e5b6eaf1b2c2ef004b8b2a8128f66
Ehsan <faizeeehsan@gmail.com>
2025-12-07 06:04:51 -0500

    Created gpu particule and win game label !

--- Game1/Scripts/win_zone.gd
+++ Game1/Scripts/win_zone.gd
@@ -1,10 +1,20 @@
 extends Area3D
 
 const PARTICLES_SCENE := preload("res://Game1/Scenes/Particles.tscn")
+@onready var win_music: AudioStreamPlayer3D = $WinMusic
+
+@onready var you_won_label: Label = $"../../CanvasLayer/YouWonLabel"
+
 var triggered := false
 
 func _ready():
 	body_entered.connect(_on_body_entered)
+
+	
+	you_won_label.visible = false
+	win_music.stop()
+
+
 
 func _on_body_entered(body):
 	if triggered:
@@ -13,12 +23,18 @@ func _on_body_entered(body):
 		return
 
 	triggered = true
+	activate_win_sequence()
+
+
+func activate_win_sequence():
+	you_won_label.visible = true
+	
+
+	win_music.play()
 	_spawn_fireworks()
+
 
 func _spawn_fireworks():
 	var fireworks := PARTICLES_SCENE.instantiate()
-
-	# Spawn fireworks slightly above the WinZone
 	fireworks.global_position = global_position + Vector3(0, 3, 0)
-
 	get_tree().current_scene.add_child(fireworks)
